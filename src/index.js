7924bda5a06cae620b61d2894ea27b206de8227e
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-07-10 20:58:08 +0200

    Added basica elements. copied over to js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("It's doing a thing. not really...")
+// Create page Content
+const contentDiv = document.querySelector("#content")
+
+//Heading and picture
+const section = document.createElement("div");
+section.classList.add("section");
+const image = document.createElement("img");
+const heading = document.createElement("h1");
+heading.innerHTML = "Made Up Meals";
+section.appendChild(image);
+section.appendChild(heading);
+contentDiv.appendChild(section);
+
+// Tagline and some fluffy words
+const tagline = document.createElement("h2");
+tagline.classList.add("tagline");
+tagline.innerHTML = "Lorem ipsum dolor sit amet";
+const para = document.createElement("p");
+para.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci recusandae fuga quos ullam vel.";
+
+contentDiv.appendChild(tagline);
+contentDiv.appendChild(para);
