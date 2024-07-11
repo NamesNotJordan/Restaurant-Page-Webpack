6ad7b6a3775474e93806c13027e992a6d59b98dc
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-07-11 15:56:19 +0200

    Added event listeners to buttons

--- src/index.js
+++ src/index.js
@@ -6,3 +6,8 @@ import createAboutContent from "./aboutContent";
 const homeButton = document.querySelector(".home-button");
 const menuButton = document.querySelector(".menu-button");
 const aboutButton = document.querySelector(".about-button");
+
+// Set event listeners
+homeButton.addEventListener("click", createHomeContent);
+menuButton.addEventListener("click", createMenuContent);
+aboutButton.addEventListener("click", createAboutContent);
