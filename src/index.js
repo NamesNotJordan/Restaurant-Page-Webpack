4ba2725018a955995639c64df155d4af91321e2d
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-07-11 15:52:49 +0200

    got the buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import createHomeContent from "./homeContent";
 import createMenuContent from "./menuContent";
 import createAboutContent from "./aboutContent";
+
+// Get nav buttons
+const homeButton = document.querySelector(".home-button");
+const menuButton = document.querySelector(".menu-button");
+const aboutButton = document.querySelector(".about-button");
