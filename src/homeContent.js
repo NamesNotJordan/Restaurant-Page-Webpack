4b17d7853586a7fd68e3041c6126a50dfd708ea9
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-09-09 20:55:43 +0200

    messing with styles. using snorlax's colour scheme

--- src/homeContent.js
+++ src/homeContent.js
@@ -8,11 +8,15 @@ export default function createHomeContent() {
     heading.innerHTML = "Made Up Meals";
     hero.appendChild(heading);
 
+    const section = document.createElement("div");
+    section.classList.add("home-section-2");
     const tagline = document.createElement("h2");
     tagline.classList.add("tagline");
     tagline.innerHTML = "Lorem ipsum dolor sit amet";
     const para = document.createElement("p");
     para.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci recusandae fuga quos ullam vel.";
-
-    contentDiv.replaceChildren(hero, tagline, para)
+    const pic = document.createElement("div");
+    pic.classList.add("fake-pic");
+    section.replaceChildren(tagline, para, pic);
+    contentDiv.replaceChildren(hero, section);
 }
