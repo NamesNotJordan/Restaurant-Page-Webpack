133849c19c856652e7d94e322a50cf413c33cc7d
Jayden Hendrickse <jayden.heberhendrickse@gmail.com>
2024-09-16 20:35:24 +0200

    Added and styled reviews and added some padding to the bottom of the menu

--- src/homeContent.js
+++ src/homeContent.js
@@ -1,3 +1,39 @@
+const review = (text, source) => {
+    let element = document.createElement("div");
+    element.classList.add("review");
+    let reviewText = document.createElement("p");
+    reviewText.classList.add("review-para");
+    reviewText.innerHTML = text;
+    
+    let reviewSource = document.createElement("p");
+    reviewSource.classList.add("review-source");
+    reviewSource.innerHTML = "- "+source;
+
+    element.appendChild(reviewText);
+    element.appendChild(reviewSource);
+    return element;
+}
+
+const reviewList = [
+    review("It's like it was made just for me in particular! Outstanding!", "All of New York"),
+    review("As an avid eater of food, I was quite satisfied with their service", "Charlie Bronco"),
+    review("I'm no food critic, but the food and/or beverages served here are in a class of their own", "A Prime minister")
+]
+
+const reviewSection = () =>{
+    let element = document.createElement("div");
+    element.classList.add("review-section");
+    element.innerHTML = "<h2>Reviews</h2>"
+
+    let reviewCollection = document.createElement("div");
+    reviewCollection.classList.add("review-collection");
+    for (const r of reviewList) {
+        reviewCollection.appendChild(r);
+    }
+    element.appendChild(reviewCollection);
+
+    return element;
+}
 
 export default function createHomeContent() {
     const contentDiv = document.querySelector("#content");
@@ -18,5 +54,5 @@ export default function createHomeContent() {
     const pic = document.createElement("div");
     pic.classList.add("fake-pic");
     section.replaceChildren(tagline, para, pic);
-    contentDiv.replaceChildren(hero, section);
+    contentDiv.replaceChildren(hero, section, reviewSection());
 }
